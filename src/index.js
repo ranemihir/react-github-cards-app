f00b5ce54bade77ef9defe4195d2608c646d57b0
ranemihir <mihirrane171@gmail.com>
2021-11-06 19:27:55 +0530

    create card component

--- src/index.js
+++ src/index.js
@@ -2,11 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 
-const App = (props) => {
-    return (
-        <div className="header">{props.title}</div>
-    );
-};
+class Card extends React.Component {
+    render() {
+        return (
+            <div className="github-profile">
+                <img src="https://via.placeholder.com/100" alt="github profile" />
+                <div className="info">
+                    <div className="name">Name here...</div>
+                    <div className="company">Company Name here...</div>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="header">{this.props.title}</div>
+                <Card />
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(
     <App title='The GitHub Cards App' />,
